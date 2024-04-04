91ffe84e198d94f47f48e08513499a92b1c390ae
t4rb0 <mstudeneckij@gmail.com>
2024-04-04 17:16:50 +0600

    profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,9 +1,37 @@
 import React from "react";
-function Profile(){
-    return(
+import profilePic from "../images/profile.jpg"
+import "./profile.css"
+function Profile() {
+    return (
         <div className="profile">
-            PROFILE
+            <div className="profile-info">
+                <img src={profilePic} className="profile-pic" />
+                <div className="user-info">
+                    <h2 className="user-info__name">Ivan Ivanov</h2>
+                    <hr></hr>
+                    <p className="user-info__about"><span>City: </span>Moscow</p>
+                    <p className="user-info__about"><span>Age: </span>20</p>
+                    <p className="user-info__about"><span>Education: </span>Moscow State University</p>
+                </div>
+            </div>
+            <div className="feed">
+                <h2 className="feed__title">Ivan's posts</h2>
+                <textarea className="feed__input" type="text" placeholder="What's new?" />
+                <br></br>
+                <button className="feed__btn">add post</button>
+                <div className="post">
+                    <div className="post__user-info">
+                        <img src={profilePic} className="post__image"></img>
+                        <p className="post__user-name">Ivan Ivanov</p>
+                        <p className="post__date">02.04.2024</p>
+                    </div>
+                    <br></br>
+                    <p className="post__text">some text</p>
+                </div>
+            </div>
         </div>
     )
 }
 export default Profile
+
+// Дописать стили профилю.
--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// git add .   - добавить файлы в систему контроля версий
+// git commit -m "сообщения"  - сохранить изменения
+// git push  - загрузить изменения на удаленный репозиторий
